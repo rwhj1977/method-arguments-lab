77245e3d15ad0f3d5b1fa3500ff97629a47addf8
Robert Hardy <rwhj77@hotmail.com>
2021-01-13 21:29:23 -0600

    commit tree f403eb5feeff4a587d5baef2ddc73d2d7ea3f1da

---
+++ lib/introduction.rb
@@ -0,0 +1,15 @@
+#introduction
+def introduction
+end
+def introduction(name)
+    puts "Hi, my name is #{name}."
+end
+def introduction_with_language(language)
+    puts "Hi my name is #{name} and I am learning to program in #{language}."
+end
+def introduction_with_language(name, language)
+    puts "Hi, my name is #{name} and I am learning to program in #{language}."
+end
+   
+
+
